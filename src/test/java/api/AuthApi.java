207e6bff1fd2c1adcadbedcdd8271b3cf727e5b6
Maria Stelmakh <maniastelmah@gmail.com>
2024-02-11 01:32:01 +0400

    initial commit

--- src/test/java/api/AuthApi.java
+++ src/test/java/api/AuthApi.java
@@ -1,9 +1,8 @@
 package api;
 
-import config.LoginConfig;
+
 import models.LoginRequest;
 import models.LoginResponse;
-import org.aeonbits.owner.ConfigFactory;
 
 import static data.ApiEndpoints.LOGIN;
 import static data.UserData.PASSWORD;
@@ -26,5 +25,5 @@ public class AuthApi {
                         .log().all()
                         .spec(successLoginResponse200)
                         .extract().as(LoginResponse.class);
-    };
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,10 +12,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
-import java.util.Objects;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
     @BeforeAll
